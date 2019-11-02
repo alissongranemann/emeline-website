d57812919c7d1fc58050b5cef686a3163b62233e
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-11-02 12:43:40 -0300

    Add logo and remove gatsby generated stuff

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,20 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+import Logo from "./logo"
+
+const Container = styled.div`
+  max-width: 150px;
+  margin: auto;
+`
+
+
+const Header = () => (
+  <header>
+    <Container>
+      <Logo />
+    </Container>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
