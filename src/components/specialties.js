16c1d24663a6f2ce54dc2e32ee7f40ffbc3ba0ea
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-11-15 20:09:55 -0200

    Add specialties content

--- src/components/specialties.js
+++ src/components/specialties.js
@@ -1,17 +1,17 @@
 import React from "react"
 import styled from "styled-components"
-import {
-  FaRegGrin,
-  FaRegHospital,
-  FaMask,
-  FaStreetView,
-  FaSyringe,
-} from "react-icons/fa"
+import { FaRegGrin, FaRegHospital } from "react-icons/fa"
+import { GiWeightLiftingUp, GiFemale } from "react-icons/gi"
 
 const Container = styled.div`
   text-align: center;
   padding: 35px 0;
   background-color: #f1f1f1;
+`
+const Title = styled.h2`
+  display: inline-block;
+  margin: 0 auto;
+  margin-bottom: 50px;
 `
 
 const List = styled.ul`
@@ -24,59 +24,30 @@ const List = styled.ul`
 `
 
 const ListItem = styled.li`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-evenly;
-  min-width: 155px;
-  padding: 8px 0;
+  width: 450px;
   margin: 25px;
-  font-size: 14px;
-  font-weight: 500;
-  line-height: 1.5;
-  box-shadow: -1px 1px 2px 0px #b1b1b1;
-  background-color: #fff;
-  border-radius: 3px;
+  text-align: left;
+
+  div {
+    display: flex;
+    align-items: center;
+    margin-bottom: 15px;
+  }
 
   svg {
-    width: 80px;
-    height: auto;
-    padding: 25px;
-    margin-bottom: 5px;
-    border-radius: 100%;
-    overflow: visible;
-    color: #fff;
-  }
-
-  p {
-    max-width: 125px;
+    min-width: 35px;
+    height: 35px;
+    margin-right: 20px;
+    color: #5a187c;
   }
 `
 
-const Title = styled.h2`
-  display: inline-block;
-  margin: 0 auto;
-  margin-bottom: 50px;
+const Subtitle = styled.h3`
+  font-size: 20px;
 `
 
-const ComportamentalIcon = styled(FaRegGrin)`
-  background-color: #ffa000;
-`
-
-const MedicalIcon = styled(FaRegHospital)`
-  background-color: #3949ab;
-`
-
-const MaskIcon = styled(FaMask)`
-  background-color: #d81b60;
-`
-
-const FunctionalIcon = styled(FaStreetView)`
-  background-color: #00796b;
-`
-
-const SyringeIcon = styled(FaSyringe)`
-  background-color: #bf360c;
+const Text = styled.p`
+  font-size: 14px;
 `
 
 const Specialties = () => (
@@ -84,24 +55,54 @@ const Specialties = () => (
     <Title>Especialidades</Title>
     <List>
       <ListItem>
-        <ComportamentalIcon />
-        <p>Comportamental</p>
+        <div>
+          <FaRegHospital />
+          <Subtitle>
+            Nutrição clínica como coadjuvante na melhora de condições de saúde
+          </Subtitle>
+        </div>
+        <Text>
+          Tratamento nutricional focado nos diagnósticos apresentados como:
+          diabetes, hipertensão, alterações de colesterol e triglicerídeos,
+          doenças gástricas, doenças intestinais, doenças tireoidianas,
+          intolerâncias e alergias alimentares, anemia e outras deficiências
+          vitamínicas e minerais.
+        </Text>
       </ListItem>
       <ListItem>
-        <MedicalIcon />
-        <p>Clínica</p>
+        <div>
+          <GiWeightLiftingUp />
+          <Subtitle>Nutrição para praticantes de atividades físicas</Subtitle>
+        </div>
+        <Text>
+          Tratamento nutricional focado na Hipertrofia, definição muscular,
+          ganho de peso, perda de peso, redução de percentual de gordura,
+          adaptação alimentar adequada aos treinos praticados, melhora de
+          disposição e rendimento.
+        </Text>
       </ListItem>
       <ListItem>
-        <MaskIcon />
-        <p>Estética</p>
+        <div>
+          <FaRegGrin />
+          <Subtitle>Nutrição e estética</Subtitle>
+        </div>
+        <Text>
+          Tratamento nutricional focado no emagrecimento, nutrição aliada aos
+          tratamentos para redução de gordura localizada, celulite e estrias,
+          adequação alimentar para períodos pré e pós cirúrgicos, para
+          tratamento capilares e de estética facial.
+        </Text>
       </ListItem>
       <ListItem>
-        <FunctionalIcon />
-        <p>Funcional</p>
-      </ListItem>
-      <ListItem>
-        <SyringeIcon />
-        <p>Doenças Crônicas Não Transmissíveis</p>
+        <div>
+          <GiFemale />
+          <Subtitle>Nutrição na saúde da mulher</Subtitle>
+        </div>
+        <Text>
+          Tratamento nutricional focado nas alterações hormonais como a tensão
+          pré-menstrual, síndrome do ovário policístico e menopausa, adaptações
+          alimentares na fase pré-gestacional, gestacional e puerpério.
+        </Text>
       </ListItem>
     </List>
   </Container>
