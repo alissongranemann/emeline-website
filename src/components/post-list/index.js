0f6531dea099e8a5398a628aaac3d753fcf65147
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-11-30 19:42:34 -0200

    Add responsiveness

--- src/components/post-list/index.js
+++ src/components/post-list/index.js
@@ -1,14 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
+import styled from "styled-components"
 
 import PostItem from "./item"
 import { Title } from "./styles"
 
+const Container = styled.article`
+  display: flex;
+  flex-wrap: wrap;
+`
+
 const PostList = ({ title, children }) => (
-  <article>
+  <Container>
     <Title>{title}</Title>
     {children}
-  </article>
+  </Container>
 )
 
 PostList.Item = PostItem
